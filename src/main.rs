ade2a76aab6b165e8c457845321b42147b1e5c2e
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2025-02-19 12:28:51 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use iced::{ widget::{button, container, slider}, * };
-use rodio::{self, source, Source};
-use std::{collections::HashMap, time::Duration};
-use std::thread;
+#![windows_subsystem = "windows"]
+
+use iced::{ widget::{button, container, slider, text, text_input}, * };
+use rodio::{self, Source};
+use std::{f32, ops::Mul, thread, time::Duration};
+
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
 enum Note { 
@@ -13,13 +15,13 @@ enum NoteLength {
 }
 
 impl NoteLength { 
-    pub fn duration_in_seconds(&self) -> f32 {
+    pub fn duration_in_seconds(&self, bpm: f32) -> f32 {
         match self {
-            NoteLength::Whole => (60.0 / 120.0) * 4.0,      
-            NoteLength::Half => (60.0 / 120.0) * 2.0,        
-            NoteLength::Quarter => 60.0 / 120.0,            
-            NoteLength::Eighth => (60.0 / 120.0) * 0.5,      
-            NoteLength::Sixteenth => (60.0 / 120.0) * 0.25,  
+            NoteLength::Whole => (60.0 / bpm) * 4.0,      
+            NoteLength::Half => (60.0 / bpm) * 2.0,        
+            NoteLength::Quarter => 60.0 / bpm,            
+            NoteLength::Eighth => (60.0 / bpm) * 0.5,      
+            NoteLength::Sixteenth => (60.0 / bpm) * 0.25,  
         }
     }
 }
@@ -27,36 +29,38 @@ impl NoteLength {
 struct RealNote { 
     note: Note, 
     length: NoteLength, 
-    octave: f32, 
+    octave: f32,
 }
 
 
 impl RealNote { 
-    pub fn base_frequencies(note: Note) -> Option<f32> { 
+    pub fn base_frequencies(note: Note) -> f32 { 
         match note {
-            Note::C => Some(16.35),
-            Note::Csharp => Some(17.32),
-            Note::D => Some(18.35),
-            Note::Dsharp => Some(19.45),
-            Note::E => Some(20.60),
-            Note::F => Some(21.83),
-            Note::Fsharp => Some(23.12),
-            Note::G => Some(24.50),
-            Note::Gsharp => Some(25.96),
-            Note::A => Some(27.50),
-            Note::Asharp => Some(29.14),
-            Note::B => Some(30.87),
-            _ => None,
+            Note::C => 16.35,
+            Note::Csharp => 17.32,
+            Note::D => 18.35,
+            Note::Dsharp => 19.45,
+            Note::E => 20.60,
+            Note::F => 21.83,
+            Note::Fsharp => 23.12,
+            Note::G => 24.50,
+            Note::Gsharp => 25.96,
+            Note::A => 27.50,   
+            Note::Asharp => 29.14,
+            Note::B => 30.87,
         }
     }
-    fn play(self) {  
+    fn play(self, bpm: f32) {  
+
         thread::spawn(move || {   
-            let frequency: f32 = Self::base_frequencies(self.note).expect("Invalid Note") * (&self.octave + 1.0);
+            let time = NoteLength::duration_in_seconds(&self.length, bpm) as u64;
+            let frequency: f32 = Self::base_frequencies(self.note).powf((&self.octave + 1.0));
             let (_stream, device) = rodio::OutputStream::try_default().unwrap();
             let source = rodio::source::SineWave::new(frequency)
-            .take_duration(Duration::new(2, 0)); 
+            .amplify(100.0)
+            .take_duration(Duration::new(time, 0));
             device.play_raw(source.convert_samples()).unwrap();
-            std::thread::sleep(Duration::new(NoteLength::duration_in_seconds(&self.length) as u64, 0));
+            std::thread::sleep(Duration::new(time, 0));
         });
     }
 }
@@ -73,12 +77,16 @@ pub fn main() -> iced::Result {
 }
 
 struct Program { 
-    slided: f32
+    octave: f32,
+    bpm: f32,
+    custom_bpm: String
 } 
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message { 
-    Sliding(f32),
+    OctaveChange(f32),
+    BpmChange(f32),
+    CustomBpmChange(String),
     Play
 }
 
@@ -86,11 +94,24 @@ impl Program {
     fn view(&self) -> Element<Message> { 
         container(widget::column![
             widget::row!(
+                text("Octave"),
                 slider(
-                    0.0..=6.0,
-                    self.slided,
-                    Message::Sliding
+                    0.0..=10.0,
+                    self.octave,
+                    Message::OctaveChange
                 ),
+            ),
+
+            widget::row!(
+                text("BPM"),
+                slider(
+                    10.0..=300.0,
+                    self.bpm, 
+                    Message::BpmChange
+                ),
+                text_input(format!("{}", &self.bpm).as_str(), &self.custom_bpm)
+                .on_input(Message::CustomBpmChange) 
+                .width(Length::Fixed(50.0)),
             ),
 
             widget::row!(
@@ -101,32 +122,48 @@ impl Program {
                 .padding(10),             
             ),
 
+            widget::row!(
+                text(format!("Octave: {}", &self.octave))
+                .size(72)
+                .wrapping(text::Wrapping::Glyph),
+            )
+
         ].spacing(10))
         .padding(10)
         .into()
     }
     fn update(&mut self, message: Message) { 
         match message { 
-            Message::Sliding(value) => {
-                self.slided = value;
+            Message::OctaveChange(value) => {
+                self.octave = value;
+            }
+
+            Message::CustomBpmChange(value) => {
+                self.bpm = value.parse().expect("Failed to convert");
+            }
+
+            Message::BpmChange(value) => {
+                self.bpm = value;
             }
 
             Message::Play => {
                 RealNote::play(RealNote{
                     note: Note::C, 
                     length: NoteLength::Whole,
-                    octave: self.slided
-                });
+                    octave: self.octave
+                }, self.bpm);
+                std::thread::sleep(Duration::new(3, 0));
                 RealNote::play(RealNote{
                     note: Note::E, 
                     length: NoteLength::Whole,
-                    octave: self.slided
-                });
+                    octave: self.octave,
+                }, self.bpm);
+                std::thread::sleep(Duration::new(3, 0));
                 RealNote::play(RealNote{
                     note: Note::G, 
                     length: NoteLength::Whole,
-                    octave: self.slided
-                });
+                    octave: self.octave,
+                }, self.bpm);
             }
         }
     }
@@ -140,7 +177,9 @@ impl Program {
 impl Default for Program { 
     fn default() -> Self {
         Self {
-            slided: 0.0
+            octave: 0.0,
+            bpm: 120.0,
+            custom_bpm: "120".to_string()
         }
     }
 }

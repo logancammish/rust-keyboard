a51239625760042e4de3059c4009af240e4b319e
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2025-02-22 17:28:44 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 
 use iced::widget::{button, container, slider, text, text_input};
 use iced::{Theme, Element, widget, Length, Subscription};
+use palette::num::Real;
 use rodio::{self, Source};
 use std::{f32, thread, time::Duration};
+use threadpool::ThreadPool;
+
 
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
@@ -11,6 +14,7 @@ enum Note {
     A, Asharp, B, C, Csharp, D, Dsharp, E, F, Fsharp, G, Gsharp
 }
 
+#[derive(Debug, Clone)]
 enum NoteLength { 
     Whole, Half, Quarter, Eighth, Sixteenth
 }
@@ -34,6 +38,7 @@ impl NoteLength {
     }
 }
 
+#[derive(Debug, Clone)]
 struct RealNote { 
     note: Note, 
     length: NoteLength, 
@@ -59,19 +64,26 @@ impl RealNote {
         }
     }
     fn play(self, bpm: f32) {  
+        let time = NoteLength::duration_in_seconds(&self.length, bpm);
+        let frequency: f32 = Self::base_frequencies(self.note) * 2_f32.powf(self.octave);
+        println!("Playing: {}hz | Time: {}s", frequency, time);
+        let (_stream, device) = rodio::OutputStream::try_default().unwrap();
+        let source = rodio::source::SineWave::new(frequency)
+        .amplify(100.0)
+        .take_duration(Duration::from_secs_f32(time));
 
-        thread::spawn(move || {   
-            let time = NoteLength::duration_in_seconds(&self.length, bpm);
-            let frequency: f32 = Self::base_frequencies(self.note) * 2_f32.powf(self.octave);
-            println!("Playing: {}hz | Time: {}s", frequency, time);
-            let (_stream, device) = rodio::OutputStream::try_default().unwrap();
-            let source = rodio::source::SineWave::new(frequency)
-            .amplify(100.0)
-            .take_duration(Duration::from_secs_f32(time));
+        let sink = rodio::Sink::try_new(&device).unwrap();
+        sink.append(source);
+        sink.sleep_until_end();
+    }
+}
 
-            let sink = rodio::Sink::try_new(&device).unwrap();
-            sink.append(source);
-            sink.sleep_until_end();
+fn async_play_note(notes: Vec<RealNote>, bpm: f32) {
+    println!("Playing notes: {:?}", notes); 
+    let pool = ThreadPool::new(notes.len());
+    for note in notes { 
+        pool.execute(move || {
+            note.play(bpm);
         });
     }
 }
@@ -185,12 +197,22 @@ impl Program {
                     length: NoteLength::Half,
                     octave: self.octave,
                 }, self.bpm);
-                //std::thread::sleep(Duration::new(2, 0));
                 RealNote::play(RealNote{
                     note: Note::G, 
                     length: NoteLength::Quarter,
                     octave: self.octave,
                 }, self.bpm);
+
+                std::thread::sleep(Duration::new(3, 0));
+
+
+                let c_chord: Vec<RealNote> = Vec::from([
+                    RealNote { note: Note::C, length: NoteLength::Whole, octave: self.octave },
+                    RealNote { note: Note::E, length: NoteLength::Half, octave: self.octave },
+                    RealNote { note: Note::G, length: NoteLength::Quarter, octave: self.octave },
+                ]);
+
+                async_play_note(c_chord, self.bpm);
             }
         }
     }
